77916cd507088690b78081b0f697429d194b192d
ThMizh21 <muthamizhanboobalan@gmail.com>
2024-09-23 16:14:55 +0530

    changes

--- js/script5.js
+++ js/script5.js
@@ -7,8 +7,8 @@ function loadGitHubHeatmap() {
     // Create an iframe element
     const iframe = document.createElement('iframe');
     <iframe src="https://github-readme-stats.vercel.app/api?username=ThMizh21" width="WIDTH" height="HEIGHT" frameborder="0"></iframe>
-    // Replace USERNAME with your GitHub username
-    iframe.style.width = '100%';
+    
+    iframe.style.width = '100%';  // Adjust width as needed
     iframe.style.height = '600px'; // Adjust height as needed
     iframe.style.border = 'none';
     
